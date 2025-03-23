be9765f92d01fb9b8a5f85be84e19e46785a2991
EdJimenezO <edward.claire01@gmail.com>
2025-03-23 08:48:45 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Puedes agregar funcionalidades adicionales con JavaScript si es necesario
+document.querySelector('button').addEventListener('click', function() {
+    alert('Búsqueda realizada');
+});
